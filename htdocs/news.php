542c5c543c94fbba6648545722845fc2fd8aaa1e
Eric Hameleers <alien@slackware.com>
2023-02-02 20:44:25 +0100

    Update the NEWS

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,53 +1,12 @@
-<b>06/Jul/2012</b><br>
-Now you can see the <a href="logupdate.txt">log of last update</a>.
-<br><br>
-<b>24/Apr/2012</b><br>
-After a very long downtime, file searcher turned up.<br/>
-Well, now I've a lot of space on my hosting, so it works!<br/>
-I've re-enabled guestbook and <a href="stats.php">statistics functions</a>.<br/>
-Good search!!
-<br/><br/>
-<!--<b>24/Jul/2011</b>-->
-<!--File searcher turned up. I'm not sure that it remaining up, but now is UP ;-)-->
-<b>26/Apr/2011</b><br/>
-You can found <a href="http://01.webuda.com/">here</a> a _VERY_VERY_OLD_ release of this file searcher (the first, I think).
-<!--but the file searching works ;-)
+<b>27/jan/2023</b><br>
+I removed the SlackAr repository after the Slackware Argentina community deleted that repository (previously at http://slack.ar/slackware/15.0/x86_64/) following a difference of opinion with the repo maintainer.<br/>
 <br>
-<b>22/Apr/2011</b><br>
-I'm working to turn UP the <b>File</b> searcher.<br>
-Please wait a while.
+<b>25/jan/2023</b><br>
+The first spammer showed up. I deleted the guest book entry but this PHP code is not using any kind of spam fighting and I am not going to add it. If more spammers show up I will have to disable the guest book.<br/>
 <br>
-<b>09/Dec/2010</b><br>
-Sorry, File searcher will be down forever :-(<br>
-Remain up the packages searcher :-)
--->
-<!--<b>28/Aug/2010</b><br>
-Sorry for down site.<br>
-I'd some problem with my server (during my vacation :-( ), so I migrated on an other provider.<br>
-Unfortunatly, I've only 200MB for database and file search need 700M or more, and a large cpu usage, so I must disable this functionality.<br>
-Until I use this server the SlakFinder 2 developing is freezed.<br>
-Also I must disable statistics function, and restart counters.<br>
+<b>10/jan/2023</b><br>
+Onboarding several new 3rd-party repositories forced me to drop the database a couple of times and modify the database scheme to deal with SQL errors.<br> Apologies for any inconvenience.<br/>
 <br>
-Good search to all!!
-
-<b>26/Jul/2010:</b><br>
-Sorry. Due limit spam I've disabled links in guestbook, so do not enter it.<br>
-I finally chose the style for the new home that is under development.<br>
-But now I go on vacation, so I cant continue to develop. Happy holidays to all!<br>
-Thanks all for your contributes!!<br>
+<b>08/jan/2023</b><br>
+Re-implementation of the slakfinder.org web site code.<br/>
 <br>
-<b>17/Jul/2010:</b><br>
-Starting develop <a href='dev/'>SlakFinder second release</a> tree. Help me.<br>
-<br>
-<b>12/Jul/2010:</b><br>
-Thanks all for using SlakFinder!!<br>
-In recent days I have been working to compact the list of repositories (already too long :-)), so it was a little space on the screen to add a board for news and a small <a href=gb.php>guestbook</a> for your feedback.<br>
-<br>
-<b>04/Jul/2010:</b><br>
-Reached about a thousand of searches.<br>
-<br>
-<b>01/Jul/2010:</b><br>
-Thanks all for use SlakFinder!! I hope it was useful to all.<br>
-Thanks to all contributors (for bug and/or repository reportage)<br>
-On behalf of all thank those who build new packages<br>
--->
