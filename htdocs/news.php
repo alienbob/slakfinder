7f8a1c25ebebffa2466fc633f3b69a2b3b63fab7
Eric Hameleers <alien@slackware.com>
2025-08-15 15:04:05 +0200

    NEWS: SLACKDCE repository was removed on 13/may/2024
    This repository was not stable and eventually disappeared completely.

--- htdocs/news.php
+++ htdocs/news.php
@@ -1,3 +1,6 @@
+<b>13/may/2024</b><br>
+I removed the SlackDCE repository (http://slackdce.radioclub.ar/) since it seems to have vanished online.</a><br/>
+<br>
 <b>21/dec/2023</b><br>
 I added the SlackDCE repository (http://slackdce.radioclub.ar/) which aims to provide a pre-built package for every entry in SlackBuilds.org - targeting 64bit Slackware 15.0. Read their manifesto at <a href="http://slackdce.radioclub.ar/slackdce/README.txt" target="_blank">http://slackdce.radioclub.ar/slackdce/README.txt</a><br/>
 <br>
